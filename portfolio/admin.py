96169fd30f0b90fef5578742ae36ac5619230f16
Ocak14 <sayfullayeva191@gmail.com>
2024-07-15 12:41:11 +0500

    portfolio7

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from .models import Contact,Blog,Portfolio,Team,PortfolioCategory
-
+from django.utils.html import format_html
 # Register your models here.
 
-admin.site.register((Contact,Blog,Portfolio,Team,PortfolioCategory)) 
+admin.site.register((Contact,Portfolio,Team,PortfolioCategory)) 
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title','author','img')
+    readonly_fields = ['slug']
+    def img(self, obj):
+         return format_html('<img width="100" height="100" src="{}" />'.format(obj.image.url))
