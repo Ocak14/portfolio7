f62f325cf4e5e1b2c915ce3116fef85e027e911b
Ocak14 <sayfullayeva191@gmail.com>
2024-07-17 09:00:22 +0500

    commit

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Contact,Blog,Portfolio,Team,PortfolioCategory
+from .models import Contact,Blog,Portfolio,Category,Team,PortfolioCategory
 from django.utils.html import format_html
+from django.db import models
 # Register your models here.
 
-admin.site.register((Contact,Portfolio,Team,PortfolioCategory)) 
+admin.site.register((Contact,Portfolio,Category,PortfolioCategory)) 
 
 @admin.register(Blog)
 class BlogAdmin(admin.ModelAdmin):
@@ -11,3 +12,14 @@ class BlogAdmin(admin.ModelAdmin):
     readonly_fields = ['slug']
     def img(self, obj):
          return format_html('<img width="100" height="100" src="{}" />'.format(obj.image.url))
+  
+
+@admin.register(Team)
+class Team(admin.ModelAdmin):
+    list_display = ('img','full_name','job')
+    readonly_fields = ['id']
+    def img(self, obj):
+         return format_html(
+    '<img width="100" height="100" src="{}" style="border-radius: 50%;" />',
+    obj.image.url
+)
